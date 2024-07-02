f41e60d18b78f4b84d93b7888b602c7c515cca3f
jmbcs <julio.m.b.c.silva+github@gmail.com>
2024-07-02 18:11:13 +0200

    updated schema with better validation and doc

--- services/restapi/api/app/schemas.py
+++ services/restapi/api/app/schemas.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 
 from pydantic import BaseModel, Field, field_validator
 
@@ -8,7 +8,7 @@ class PlatformSchema(BaseModel):
     Schema representing the details of a platform.
 
     Attributes:
-        name (str): The name of the platform.
+        name (str): The name of the platform.\n
     """
 
     name: str = Field(description="The name of the platform.")
@@ -19,7 +19,7 @@ class PublisherSchema(BaseModel):
     Schema representing the details of a publisher.
 
     Attributes:
-        name (str): The name of the publisher.
+        name (str): The name of the publisher.\n
     """
 
     name: str = Field(description="The name of the publisher.")
@@ -30,7 +30,7 @@ class DeveloperSchema(BaseModel):
     Schema representing the details of a developer.
 
     Attributes:
-        name (str): The name of the developer.
+        name (str): The name of the developer.\n
     """
 
     name: str = Field(description="The name of the developer.")
@@ -41,44 +41,44 @@ class GameSchema(BaseModel):
     Schema representing the details of a game.
 
     Attributes:
-        title (str): The title of the game.
-        genre (str): The genre of the game.
-        release_date (str): The release date of the game in the format YYYY-MM-DD.
-        platform (str): The name of the platform on which the game is available.
-        publisher (str): The name of the publisher of the game.
-        developer (str): The name of the developer of the game.
+        title (str): The title of the game.\n
+        genre (str): The genre of the game.\n
+        release_date (date): The release date of the game.\n
+        platform (str): The name of the platform on which the game is available.\n
+        publisher (str): The name of the publisher of the game.\n
+        developer (str): The name of the developer of the game.\n
     """
 
     title: str = Field(description="The title of the game.")
     genre: str = Field(description="The genre of the game.")
-    release_date: str = Field(
-        description="The release date of the game in the format YYYY-MM-DD."
-    )
+    release_date: date = Field(description="The release date of the game.")
 
     platform: str = Field(
-        None, description="The name of the platform on which the game is available."
+        description="The name of the platform on which the game is available."
     )
-    publisher: str = Field(None, description="The name of the publisher of the game.")
-    developer: str = Field(None, description="The name of the developer of the game.")
+    publisher: str = Field(description="The name of the publisher of the game.")
+    developer: str = Field(description="The name of the developer of the game.")
 
     @field_validator("release_date")
     def validate_release_date(cls, v):
         """
-        Validate the release date to ensure it is in the correct format and not in the future.
+        Validate the release date to ensure it is not in the future.
 
         Args:
-            v (str): The release date to validate.
+            v (date): The release date to validate.\n
 
         Raises:
-            ValueError: If the date is not in the format YYYY-MM-DD or is in the future.
+            ValueError: If the date is in the future.\n
 
         Returns:
-            str: The validated release date.
+            date: The validated release date.\n
         """
-        try:
-            release_date = datetime.strptime(v, "%Y-%m-%d")
-        except ValueError:
-            raise ValueError("Release date must be in the format YYYY-MM-DD.")
-        if release_date > datetime.now():
+        if v > date.today():
             raise ValueError("Release date cannot be in the future.")
         return v
+
+
+# Pydantic model for the response message
+class GameCreateResponse(BaseModel):
+    message: str = "Game created successfully."
+    game: GameSchema
