5c9766627484c570a4c290e251b9f06e18f17b60
jmbcs <julio.m.b.c.silva+github@gmail.com>
2024-07-02 18:12:08 +0200

    updated created game with better error handling, documentation

--- services/restapi/api/app/routers/game.py
+++ services/restapi/api/app/routers/game.py
@@ -1,7 +1,8 @@
 import logging
 
 from api.app.models import Developer, Game, Platform, Publisher
-from api.app.schemas import GameSchema
+from api.app.responses import create_game_responses
+from api.app.schemas import GameCreateResponse, GameSchema
 from api.database.db import get_db
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
@@ -10,59 +11,96 @@ router: APIRouter = APIRouter(tags=["GuestReady Challenge"], prefix="/guestready
 logger: logging.Logger = logging.getLogger(__name__)
 
 
-@router.post("/game", status_code=status.HTTP_201_CREATED)
-async def create_game(request: GameSchema, db: Session = Depends(get_db)):
+@router.post(
+    "/game",
+    status_code=status.HTTP_201_CREATED,
+    response_model=GameCreateResponse,
+    responses=create_game_responses,
+)
+async def create_game(
+    game: GameSchema, db: Session = Depends(get_db)
+) -> GameCreateResponse:
     try:
-        with db.begin():
-            # Check or create platform
-            platform = (
-                db.query(Platform).filter(Platform.name == request.platform).first()
+        # VALIDATION - Check if the game already exists
+        existing_game: Game | None = (
+            db.query(Game)
+            .filter(
+                Game.title == game.title,
             )
-            if not platform:
-                platform = Platform(name=request.platform)
-                db.add(platform)
-                db.flush()
-            logger.debug(f"Platform: {platform}")
-
-            # Check or create publisher
-            publisher = (
-                db.query(Publisher).filter(Publisher.name == request.publisher).first()
+            .first()
+        )
+        if existing_game:
+            logger.debug(f"Game already exists: {existing_game}")
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail={
+                    "message": "A game with the same title already exists.",
+                },
             )
-            if not publisher:
-                publisher = Publisher(name=request.publisher)
-                db.add(publisher)
-                db.flush()
-            logger.debug(f"Publisher: {publisher}")
 
-            # Check or create developer
-            developer = (
-                db.query(Developer).filter(Developer.name == request.developer).first()
-            )
-            if not developer:
-                developer = Developer(name=request.developer)
-                db.add(developer)
-                db.flush()
+        # Check if platform already exists, if not create it.
+        platform: Platform | None = (
+            db.query(Platform).filter(Platform.name == game.platform).first()
+        )
+        if not platform:
+            platform = Platform(name=game.platform)
+            db.add(platform)
+            db.flush()
+            logger.debug(f"Created new platform: {platform}")
+        else:
+            logger.debug(f"Found existing platform: {platform}")
 
-            logger.debug(f"Developer: {developer}")
+        # Check if publisher already exists, if not create it.
+        publisher: Publisher | None = (
+            db.query(Publisher).filter(Publisher.name == game.publisher).first()
+        )
+        if not publisher:
+            publisher = Publisher(name=game.publisher)
+            db.add(publisher)
+            db.flush()
+            logger.debug(f"Created new publisher: {publisher}")
+        else:
+            logger.debug(f"Found existing publisher: {publisher}")
 
-            # Create game
-            game = Game(
-                title=request.title,
-                genre=request.genre,
-                platform=platform,
-                publisher=publisher,
-                developer=developer,
-                release_date=request.release_date,
-            )
-            db.add(game)
-            db.commit()
-            db.refresh(game)
+        # Check if developer already exists, if not create it.
+        developer: Developer | None = (
+            db.query(Developer).filter(Developer.name == game.developer).first()
+        )
+        if not developer:
+            developer = Developer(name=game.developer)
+            db.add(developer)
+            db.flush()
+            logger.debug(f"Created new developer: {developer}")
+        else:
+            logger.debug(f"Found existing developer: {developer}")
 
-        return {"message": "Game created successfully", "game": game}
+        # Create game
+        new_game: Game = Game(
+            title=game.title,
+            genre=game.genre,
+            platform=platform,
+            publisher=publisher,
+            developer=developer,
+            release_date=game.release_date,
+        )
+
+        db.add(new_game)
+        db.commit()
+        db.refresh(new_game)
+        logger.debug(f"Created new game: {new_game}")
+
+        return GameCreateResponse(
+            game=game,
+        )
+
+    except HTTPException as http_exc:
+        db.rollback()
+        logger.error(f"HTTP error occurred: {http_exc.detail}")
+        raise http_exc
 
     except Exception as e:
+        db.rollback()
         logger.error(f"An error occurred: {e}")
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail="Internal Server Error",
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
         )
